b590bd399239782aa21807a31f58188016841a8d
DanThePanMan <danielchenschw@gmail.com>
2025-09-07 05:16:45 -0400

    Update to fix build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,12 @@
 import type { Config } from "tailwindcss";
 
-const svgToDataUri = require("mini-svg-data-uri");
-import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
-
-function addVariablesForColors({ addBase, theme }: any) {
-    const allColors = flattenColorPalette(theme("colors"));
-    const newVars = Object.fromEntries(
-        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-    );
-
-    addBase({
-        ":root": newVars,
-    });
-}
-
 export default {
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
+    darkMode: "class",
     theme: {
         extend: {
             animation: {
@@ -31,29 +18,25 @@ export default {
                     "0%": { transform: "translateX(-200px)" },
                     "100%": { transform: "translateX(200px)" },
                 },
+                aurora: {
+                    from: {
+                        backgroundPosition: "50% 50%, 50% 50%",
+                    },
+                    to: {
+                        backgroundPosition: "350% 50%, 350% 50%",
+                    },
+                },
             },
             colors: {
                 background: "var(--background)",
                 foreground: "var(--foreground)",
             },
+            backgroundImage: {
+                "dot-thick-neutral-300": `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='16' height='16' fill='none'%3e%3ccircle fill='%23d1d5db' id='pattern-circle' cx='10' cy='10' r='2.5'%3e%3c/circle%3e%3c/svg%3e")`,
+                "dot-thick-neutral-800": `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='16' height='16' fill='none'%3e%3ccircle fill='%23374151' id='pattern-circle' cx='10' cy='10' r='2.5'%3e%3c/circle%3e%3c/svg%3e")`,
+                "dot-thick-indigo-500": `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='16' height='16' fill='none'%3e%3ccircle fill='%236366f1' id='pattern-circle' cx='10' cy='10' r='2.5'%3e%3c/circle%3e%3c/svg%3e")`,
+            },
         },
     },
-    plugins: [
-        addVariablesForColors,
-        function ({ matchUtilities, theme }: any) {
-            matchUtilities(
-                {
-                    "bg-dot-thick": (value: any) => ({
-                        backgroundImage: `url("${svgToDataUri(
-                            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
-                        )}")`,
-                    }),
-                },
-                {
-                    values: flattenColorPalette(theme("backgroundColor")),
-                    type: "color",
-                }
-            );
-        },
-    ],
+    plugins: [],
 } satisfies Config;
